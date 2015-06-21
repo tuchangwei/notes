b78730f1f30bb0311bb493d9b20692c34890d85d
vale <changweitu@gmail.com>
2015-06-21 22:41:29 +0800

    Add the ActiveAndroid to project.

---
+++ app/src/main/java/io/github/tuchangwei/notes/Application.java
@@ -0,0 +1,19 @@
+package io.github.tuchangwei.notes;
+
+import com.activeandroid.ActiveAndroid;
+
+/**
+ * Created by vale on 6/21/15.
+ */
+public class Application extends com.activeandroid.app.Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        //清除数据库
+        //ActiveAndroid.dispose();
+        //初始化数据库
+        ActiveAndroid.initialize(this);
+    }
+}
